3d7ab6dd5db245372d019f1b783eefbad3ccac28
Ivaav <mauricio.v27@outlook.com>
2024-04-10 19:28:22 -0300

    utilização do routeProvider no main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './assets/css/reset.css'
+
+import { RouterProvider } from 'react-router-dom'
+import routers from './router.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
-    <App />
+    <RouterProvider router={routers}></RouterProvider>
   
 )
