8bf3d6242b0f5f25069ed81c10d47e2617d28879
Ivaav <mauricio.v27@outlook.com>
2024-04-14 17:37:18 -0300

    adição do icon UI nos cards de trilha

--- src/components/cardTrilha/CardTrilha.jsx
+++ src/components/cardTrilha/CardTrilha.jsx
@@ -1,22 +1,41 @@
-import './CardTrilha.css'
+import { useState } from 'react';
+import styles from './CardTrilha.module.css'
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import StarBorderIcon from '@mui/icons-material/StarBorder';
+
 
 
 export default function CardTrilha(props){
   let {nome, imagem, local, duracao, trajeto, nivel,autor} = props.trilhas
-  
+  const [favorite, setFavorite] = useState(false)
+  function handleClickFavorite(){
+    setFavorite(!favorite)
+  }
+
   return(
-        <div className="content">
-          <img src={imagem} className="img-trilha" />
-          <div className="description">
-            <h1 className='title mb16'>{nome} - {local}</h1>
-            <div className="autor">
+        <div className={styles.content}>
+          <img src={imagem} className={styles.img} />
+          <div className={styles.description}>
+            <div className={`${styles.cardHeader} ${styles.mb16}`}>
+            <h1>{nome} - {local}</h1> 
+            <button onClick={() => handleClickFavorite()}>{favorite ? <FavoriteIcon/>: <FavoriteBorderIcon/>}</button>
+            </div>
+            <div className={styles.autor}>
               <h1>Por: {autor}</h1>
             </div>
-            <h3 className="mb16">Duração: {duracao}min</h3>
-            <h3 className="mb16">Trajeto: {trajeto}km</h3>
-            <h4 className="mb16 level">{nivel}</h4>
-            <div className="avaliacoes">
-                <a href="/">Avaliações</a>
+            <h3 className={styles.mb16}>Duração: {duracao}min</h3>
+            <h3 className={styles.mb16}>Trajeto: {trajeto}km</h3>
+            <h4 className={`${styles.level} ${styles.mb16}`}>{nivel}</h4>
+            <div className={styles.review}>
+              <div className="stars">
+                <button><StarBorderIcon/></button>
+                <button><StarBorderIcon/></button>
+                <button><StarBorderIcon/></button>
+                <button><StarBorderIcon/></button>
+                <button><StarBorderIcon/></button>
+              </div>
+              <span>Avaliações</span>
             </div>
           </div>
         </div>
