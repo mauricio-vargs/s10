ba0daace977d3fc805061111ba3c0cbab6e76c27
Ivaav <mauricio.v27@outlook.com>
2024-04-23 18:26:10 -0300

    criação do theme provider

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,24 @@ import Header from "./components/header/Header"
 import {Outlet} from "react-router-dom"
 import styles from "./app.module.css"
 import {TrilhasContextProvider} from "./context/TrilhasContext"
-
+import { theme } from "./providers/ThemeProvider"
+import { ThemeProvider } from "@emotion/react"
 
 
 
 export default function App(){
 
   return(
-    <TrilhasContextProvider>
-      <div className={styles.container}>
-        <Header></Header>
-        <Outlet></Outlet>
-        <Footer></Footer>
-      </div>
-    </TrilhasContextProvider>
+    <ThemeProvider theme={theme}>
+
+      <TrilhasContextProvider>
+        <div className={styles.container}>
+          <Header></Header>
+          <Outlet></Outlet>
+          <Footer></Footer>
+        </div>
+      </TrilhasContextProvider>
+    </ThemeProvider>
   )  
 
 }
