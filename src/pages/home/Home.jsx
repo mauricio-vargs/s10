cf91c04847b004423974068befd1479878c52cc9
Ivaav <mauricio.v27@outlook.com>
2024-04-23 18:30:05 -0300

    página home

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import LowerHome from "../../components/lowerHome";
+import UpperHome from "../../components/upperHome";
+
+export default function Home(){
+    
+    return (
+            <>
+                <UpperHome/>
+                <LowerHome/>
+            </>
+                
+
+    )
+}
+
